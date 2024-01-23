a0c7e35e10bc004e53cdf2c99c693143d1285615
YWSIK <yuusik818@gmail.com>
2024-01-24 05:06:21 +0900

    #4 feat: token 저장

--- src/main/java/com/gdsc/solutionchallenge/global/config/WebSecurityConfig.java
+++ src/main/java/com/gdsc/solutionchallenge/global/config/WebSecurityConfig.java
@@ -4,6 +4,8 @@ import com.gdsc.solutionchallenge.global.exception.AuthenticationEntryPointHandl
 import com.gdsc.solutionchallenge.global.jwt.JwtAuthenticationFilter;
 import com.gdsc.solutionchallenge.global.jwt.JwtTokenUtil;
 import com.gdsc.solutionchallenge.oauth.CustomOAuth2UserService;
+import com.gdsc.solutionchallenge.oauth.HttpCookieOAuth2AuthorizationRequestRepository;
+import com.gdsc.solutionchallenge.oauth.OAuth2AuthenticationSuccessHandler;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,8 +17,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 @Configuration
 @AllArgsConstructor
 @EnableWebSecurity
@@ -25,7 +25,12 @@ public class WebSecurityConfig {
     private final JwtTokenUtil jwtTokenUtil;
     private final AuthenticationEntryPointHandler authenticationEntryPointHandler;
     private final CustomOAuth2UserService customOAuth2UserService;
+    private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
 
+    @Bean
+    public HttpCookieOAuth2AuthorizationRequestRepository cookieOAuth2AuthorizationRequestRepository() {
+        return new HttpCookieOAuth2AuthorizationRequestRepository();
+    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
         http
@@ -36,8 +41,7 @@ public class WebSecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
-                        .requestMatchers("/jwt/**").permitAll()
-                        .requestMatchers("/login/**").permitAll()
+                        .requestMatchers("/jwt/**", "/oauth2/**", "/login/**").permitAll()
                         .requestMatchers( "/","/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
@@ -46,9 +50,23 @@ public class WebSecurityConfig {
                 // JwtAuthenticationFilter를 UsernamePasswordAuthenticationFilter 전에 넣는다
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenUtil), UsernamePasswordAuthenticationFilter.class)
 
-                .oauth2Login(oauth2Login ->
-                        oauth2Login.userInfoEndpoint(userInfoEndpointConfig ->
-                                userInfoEndpointConfig.userService(customOAuth2UserService)));
+                //oauth2
+                .oauth2Login(oauth2Login -> oauth2Login
+                        .authorizationEndpoint(authorizationEndpoint ->
+                                authorizationEndpoint
+//                                        .baseUri("/oauth2/authorize")
+                                        .authorizationRequestRepository(cookieOAuth2AuthorizationRequestRepository())
+                        )
+                        .redirectionEndpoint(redirectionEndpoint ->
+                                redirectionEndpoint
+                                        .baseUri("/login/oauth2/code/**")
+                        )
+                        .userInfoEndpoint(userInfoEndpoint ->
+                                userInfoEndpoint
+                                        .userService(customOAuth2UserService)
+                        )
+                        .successHandler(oAuth2AuthenticationSuccessHandler)
+                );
 
         return http.build();
     }
