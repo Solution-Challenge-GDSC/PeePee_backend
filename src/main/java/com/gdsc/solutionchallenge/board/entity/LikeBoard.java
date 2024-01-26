cd56182e1f0330fb8a55b324060d72db17dfd595
이다빈 <dlekqls6204@naver.com>
2024-01-27 02:59:54 +0900

    #6 feat: 게시글 좋아요/좋아요 취소 기능 구현

---
+++ src/main/java/com/gdsc/solutionchallenge/board/entity/LikeBoard.java
@@ -0,0 +1,30 @@
+package com.gdsc.solutionchallenge.board.entity;
+
+import com.gdsc.solutionchallenge.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class LikeBoard {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+    @Builder
+    public LikeBoard(User user, Board board){
+        this.user = user;
+        this.board = board;
+    }
+}
