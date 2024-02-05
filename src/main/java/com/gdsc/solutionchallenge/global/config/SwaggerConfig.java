97e291250dcaf0c1d57b229ab8116b3fda1f291d
이다빈 <dlekqls6204@naver.com>
2024-02-05 20:36:47 +0900

    #13 refactor: Board 관련 수정

--- src/main/java/com/gdsc/solutionchallenge/global/config/SwaggerConfig.java
+++ src/main/java/com/gdsc/solutionchallenge/global/config/SwaggerConfig.java
@@ -2,13 +2,36 @@ package com.gdsc.solutionchallenge.global.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
-        info=@Info(title="Boiler-plate API 명세서",
-        description = "Springboot Boiler-plate API 명세서",
-        version = "v1")
+        info = @Info(
+                title = "Boiler-plate API 명세서",
+                description = "Springboot Boiler-plate API 명세서",
+                version = "v1"
+        )
 )
 @Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        String jwt = "JWT";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt); // 헤더에 토큰 포함
+        Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
+                .name(jwt)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+        );
+
+        return new OpenAPI()
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
 }
